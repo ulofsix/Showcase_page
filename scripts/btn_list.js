2a12d565a3d440ab196d969bb5ae03bd0c30d0c0
ulofsix <gba10825@gmail.com>
2021-04-14 19:33:09 +0800

    add_0414

--- scripts/btn_list.js
+++ scripts/btn_list.js
@@ -1,10 +1,9 @@
 $(function () {
+    // 切換背景用
     $('button#change_btn').click(function (e) { 
         console.log("change_btn " + this +"click");
         $('div#filter').toggleClass("filter");
-        $('div#filter').toggleClass("filter_2");
-        
-        
+        $('div#filter').toggleClass("filter_2");        
     });
     
 });
